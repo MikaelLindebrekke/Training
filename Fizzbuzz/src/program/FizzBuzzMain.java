d3b5f1c847b15f74b456f18a94fbdea22f035d34
Mikael Lindebrekke <lindebrekkem@gmail.com>
2020-10-20 20:51:08 +0200

    Changed from 30 to 100

--- Fizzbuzz/src/program/FizzBuzzMain.java
+++ Fizzbuzz/src/program/FizzBuzzMain.java
@@ -17,7 +17,7 @@ public class FizzBuzzMain {
 	
 	public static void main(String[] args) { 
 		
-		for (int i = 1; i <= 30; i++)  {
+		for (int i = 1; i <= 100; i++)  {
 			
 			if (i % FIZZBUZZ == 0) {
 				System.out.println("FizzBuzz");
